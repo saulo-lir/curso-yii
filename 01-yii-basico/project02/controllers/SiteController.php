4dfae500f631345e7dc884397fc087cbb93799d2
Saulo Lira <saulo@id5.com.br>
2019-04-26 15:12:06 -0300

    adds session and cookies

--- 01-yii-basico/project02/controllers/SiteController.php
+++ 01-yii-basico/project02/controllers/SiteController.php
@@ -125,4 +125,72 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+
+    public function actionTeste()
+    {
+        $request = Yii::$app->request;
+
+        // Pegar as requisições POST
+        $post = $request->post();
+
+        // Pegar as requisições GET
+        $get = $request->get();
+
+        // Pegar a url que estamos que estamos acessando
+        echo $request->url;
+
+        // Pegar a url comleta que estamos que estamos acessando
+        echo $request->absoluteUrl;
+
+    }
+
+    /* Trabalhando com Sessions */
+
+    public function actionCriar()
+    {
+        // Instanciando uma session
+        $session = Yii::$app->session;
+
+        // Inicializando
+        $session->open();
+
+        // Verificar se a section está ativa
+        var_dump($session->isActive);
+
+        // Criando uma section
+        $session->set('campo', 'valor');
+
+        // Exibir mensagem instantânea quando uma página é carregada. Ao ser mostrada a mensagem, ela será destruída da sessão
+        $session->setFlash('msg', 'Sucesso ao concluir');
+
+        // Fechar a sessão
+        $session->close();
+
+        /* Cookies */
+
+        // Instanciando um cookie
+        $cookies = Yii::$app->response->cookies;
+        $cookies->add(new \yii\web\Cookie([
+            'name' => 'MeuCookie',
+            'value' => 'Teste123',
+            'expire' => strtotime('+10 seconds')
+        ]));
+        
+    }
+
+    // Acessar uma sessão ou cookie que já foram criados anteriormente
+    public function actionLer()
+    {
+        $session = Yii::$app->session;
+        $cookies = Yii::$app->request->cookies;
+
+        // Ler um cookie
+        echo $cookies->get('MeuCookie');
+
+        // Ler uma sessão
+        $session->open();
+        echo $session->get('campo');
+        echo $session->getFlash('msg');
+    }
+
 }
